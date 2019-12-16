5a8306b679ed5987cc7b179a07f8c204e7d07c4b
Olyapugovi4ka <olyapugovi4ka@gmail.com>
2019-12-16 21:05:33 +0300

    Create a progect

---
+++ PhotosLibrary/PhotosLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotosLibrary
+//
+//  Created by Olga Melnik on 16.12.2019.
+//  Copyright © 2019 Olga Melnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
